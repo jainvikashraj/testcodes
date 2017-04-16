e25401fa05e01a51539d3713daf4dbfc2fa6d098
Vikash Jain <jainvikashraj@gmail.com>
2017-04-16 17:28:07 +0530

    sqrt without in built function

---
+++ sqrt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import decimal
+import math
+
+def msqrt(n):
+    if n == 0 or n == 1:
+	return n
+
+    with decimal.localcontext() as ctx:
+        x, prior = decimal.Decimal(n), None
+        while x != prior: 
+            prior = x
+            x = (x + n/x) / 2 # quadratic convergence 
+
+    return (+x) 
+
+decimal.getcontext().prec = 20 # desirable precision
+print int(math.floor(msqrt(338727899)))
